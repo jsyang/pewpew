9fe7645e5a886e4464b9a9cd1cce467613f81503
Jack Wilsdon <jack.wilsdon@gmail.com>
2016-08-19 00:56:43 +0100

    Remove loader

--- loadPewPew.js
+++
@@ -1,1 +0,0 @@
-javascript:{var u='aHR0cHM6Ly9yYXdnaXQuY29tL2ppbS10aGlzcGxhY2UvcGV3cGV3L21hc3Rlci9wZXdwZXcubWluLmpz';var d=document,ddE=d.documentElement,cE=d.createElement.bind(d),s=cE(atob("c2NyaXB0"));s.src=atob(u),ddE.appendChild(s);};void(0);
